b042208e8efa5285681a809f8800f86392409728
AquilinoPascual <=>
2022-06-10 19:13:30 +0200

    RemoveReverseAndMethodsTest and getInfoMethodModified

--- src/test/java/com/practica/cajanegra/getInfoMethodsTest.java
+++ src/test/java/com/practica/cajanegra/getInfoMethodsTest.java
@@ -24,26 +24,32 @@ public class getInfoMethodsTest {
     @MethodSource("getAPosTestArguments")
     void getAPosTest(SingleLinkedListImpl<String> base, String s, int p){
 
-        try {
             assertEquals(s,base.getAtPos(p));
-        }catch(IllegalArgumentException e) {
-            assertThrows(IllegalArgumentException.class, ()-> {
-                base.getAtPos(p);
-            });
-        }
 
     }
     static Stream<Arguments>getAPosTestArguments(){
         return Stream.of(
-                Arguments.of(base,"A",-1),
-                Arguments.of(base,"A",0),
                 Arguments.of(base,"A",1),
                 Arguments.of(base,"B",2),
                 Arguments.of(base,"C",3),
                 Arguments.of(base,"E",base.size()-1),
-                Arguments.of(base,"F",base.size()),
-                Arguments.of(base,"F",base.size()+1),
-                Arguments.of(base,"F",base.size()+2)
+                Arguments.of(base,"F",base.size())
+        );
+    }
+    @DisplayName("getAPosTestThrowException")
+    @ParameterizedTest
+    @MethodSource("getAPosTestArgumentsThrowException")
+    void getAPosTestThrowException(SingleLinkedListImpl<String> base, int p){
+
+        assertThrows(IllegalArgumentException.class,() ->base.getAtPos(p));
+
+    }
+    static Stream<Arguments>getAPosTestArgumentsThrowException(){
+        return Stream.of(
+                Arguments.of(base,-1),
+                Arguments.of(base,0),
+                Arguments.of(base,base.size()+1),
+                Arguments.of(base,base.size()+2)
         );
     }
 
@@ -54,48 +60,63 @@ public class getInfoMethodsTest {
     @ParameterizedTest
     @MethodSource("indexOfTestArguments")
     void indexOfTest(SingleLinkedListImpl<String> baseRep,String s,int p){
-
-        try {
             assertEquals(baseRep.indexOf(s),p);
-        }catch(NoSuchElementException e) {
-            assertThrows(NoSuchElementException.class, ()-> {
-                baseRep.indexOf(s);
-            });
-        }
 
     }
     static Stream<Arguments>indexOfTestArguments(){
         return Stream.of(
-                Arguments.of(baseRep,"@",1),
                 Arguments.of(baseRep,"A",1),
                 Arguments.of(baseRep,"B",4),
                 Arguments.of(baseRep,"C",7),
                 Arguments.of(baseRep,"E",13),
-                Arguments.of(baseRep,"F",16),
-                Arguments.of(baseRep,"G",6)
+                Arguments.of(baseRep,"F",16)
 
         );
-
-
-
     }
+    @DisplayName("indexOfTestThrowException")
+    @ParameterizedTest
+    @MethodSource("indexOfTestArgumentsThrowException")
+    void indexOfTestThrowException(SingleLinkedListImpl<String> baseRep,String s){
+            assertThrows(NoSuchElementException.class, ()-> {baseRep.indexOf(s);});
+    }
+    static Stream<Arguments>indexOfTestArgumentsThrowException(){
+        return Stream.of(
+                Arguments.of(baseRep,"@"),
+                Arguments.of(baseRep,"G")
+
+        );
+    }
+
+
     @DisplayName("isEmptyTest")
     @ParameterizedTest
     @MethodSource("isEmptyTestArguments")
-    void isEmptyTest(){}
+    void isEmptyTest(SingleLinkedListImpl<String> test, boolean expected){
+        assertEquals(expected,test.isEmpty());
+    }
     static Stream<Arguments>isEmptyTestArguments(){
         return Stream.of(
-                Arguments.of()
+                Arguments.of(new SingleLinkedListImpl<String>(),true),
+                Arguments.of(new SingleLinkedListImpl<String>("hola"),false),
+                Arguments.of(baseRep,false)
         );
     }
+
+    //Para valores medios no funciona
     @DisplayName("isSublistTest")
     @ParameterizedTest
     @MethodSource("isSublistTestArguments")
-    void isSublistTest(){}
+    void isSublistTest(SingleLinkedListImpl<String> baseRep,SingleLinkedListImpl<String> part, int expected){
+        assertEquals(expected,baseRep.isSubList(part));
+    }
     static Stream<Arguments>isSublistTestArguments(){
         return Stream.of(
-                Arguments.of()
-        );
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>("hola"),-1),
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>("A","A","A"),1),
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>("A","A","A","B","B","B","C","C","C","D","D","D","E","E","E","F","F","F"),1),
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>("A","A","B","B"),2),
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>("F","F","F"),18),
+                Arguments.of(baseRep,new SingleLinkedListImpl<String>(),0));
     }
 
 }
--- src/test/java/com/practica/cajanegra/removeReverseMethodsTest.java
+++ src/test/java/com/practica/cajanegra/removeReverseMethodsTest.java
@@ -1,40 +1,100 @@
 package com.practica.cajanegra;
 
+import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedList;
+import com.cajanegra.SingleLinkedListImpl;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.NoSuchElementException;
 import java.util.stream.Stream;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
 public class removeReverseMethodsTest {
+
+    //Valores repetidos y marcados para comprobar que si coge los ultimos valores en los removelast
+    private static SingleLinkedListImpl baseOrdered = new SingleLinkedListImpl<>("1", "A", "1", "2", "A", "2", "3", "A", "3", "4", "A", "4");
+    private static SingleLinkedListImpl baseOrderedExpected = new SingleLinkedListImpl<>("1", "A", "1", "2", "A", "2", "3", "A", "3", "4", "4");
+    private static SingleLinkedListImpl basicOrder = new SingleLinkedListImpl<>("1", "2", "3", "4");
 
 
     @DisplayName("removeLastTest")
     @ParameterizedTest
     @MethodSource("removeLastTestArguments")
-    void removeLastTest(){}
-    static Stream<Arguments> removeLastTestArguments(){
+    void removeLastTest(SingleLinkedListImpl<String> test, String expected) {
+        try {
+            assertEquals(expected, test.removeLast());
+        } catch (EmptyCollectionException e) {
+            e.printStackTrace();
+        }
+    }
+
+    static Stream<Arguments> removeLastTestArguments() {
         return Stream.of(
-                Arguments.of()
+                Arguments.of(baseOrdered, "4"),
+                Arguments.of(new SingleLinkedListImpl<String>("Hola"), "Hola")
         );
     }
+
+    @DisplayName("removeLastTestThrowException")
+    @Test
+    void removeLastTestThrowException() {
+        SingleLinkedListImpl<String> empty = new SingleLinkedListImpl<>();
+        assertThrows(EmptyCollectionException.class, () -> empty.removeLast());
+    }
+
+
+    // FALLA PORQUE ELIMINA TODAS LAS APARICIONES, NO SOLO LA ÚLTIMA
     @DisplayName("removeLastOfElemTest")
     @ParameterizedTest
     @MethodSource("removeLastOfElemTestArguments")
-    void removeLastOfElemTest(){}
-    static Stream<Arguments> removeLastOfElemTestArguments(){
+    void removeLastOfElemTest(SingleLinkedListImpl<String> test, SingleLinkedListImpl<String> expected, String toRemove) {
+        try {
+            assertEquals(toRemove, test.removeLast(toRemove));
+        } catch (EmptyCollectionException e) {
+            e.printStackTrace();
+        }
+        assertEquals(expected.toString(), test.toString());
+    }
+
+    static Stream<Arguments> removeLastOfElemTestArguments() {
         return Stream.of(
-                Arguments.of()
+                Arguments.of(baseOrdered, baseOrderedExpected, "A"),
+                Arguments.of(new SingleLinkedListImpl<String>("hola"), new SingleLinkedListImpl<String>(), "hola")
         );
     }
-    @DisplayName("reverseTest")
-    @ParameterizedTest
-    @MethodSource("reverseTestArguments")
-    void reverseTest(){}
-    static Stream<Arguments> reverseTestArguments(){
-        return Stream.of(
-                Arguments.of()
-        );
+
+    @DisplayName("removeLastOfElemTestThrowEmptyCollectionException")
+    @Test
+    void removeLastOfElemTestThrowEmptyCollectionException() {
+        assertThrows(EmptyCollectionException.class, () -> new SingleLinkedListImpl<String>().removeLast("hola"));
+
+    }
+
+    //No lanza exception cuando el elemento a eliminar no está en la lista
+    @DisplayName("removeLastOfElemTestThrowEmptyCollectionException")
+    @Test
+    void removeLastOfElemTestThrowNoSuchElementException() {
+        assertThrows(NoSuchElementException.class, () -> baseOrdered.removeLast("No estoy"));
+
+    }
+
+    @DisplayName("reverseTestReturn")
+    @Test
+    void reverseTestReturn() {
+        assertEquals(new SingleLinkedListImpl<String>("4", "3", "2", "1").toString(), (basicOrder.reverse()).toString());
+
+    }
+
+    @DisplayName("reverseTestList")
+    @Test
+    void reverseTestList() {
+        basicOrder.reverse();
+        assertEquals(new SingleLinkedListImpl<String>("1", "2", "3", "4").toString(), basicOrder.toString());
     }
 }
